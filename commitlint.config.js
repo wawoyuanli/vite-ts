e29a4a03669b5e44e341421a8ef5c4b9facf11a7
wawoyuanli <1583649818@qq.com>
2022-03-09 17:12:45 +0800

    update

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,8 @@
 // commitlint.config.js
-
 module.exports = {
-    // ↓忽略包含init的提交消息
+    //忽略包含init的提交消息
     ignores: [(commit) => commit.includes('init')],
-    // ↓按照传统消息格式来验证
+    //按照传统消息格式来验证
     extends: ['@commitlint/config-conventional'],
     // 自定义解析器
     parserPreset: {
